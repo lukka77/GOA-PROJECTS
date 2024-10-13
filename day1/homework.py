f578a69b687eec952e9694e79e15b75b27f6af9a
lukka77 <gorgadzeluka509@gmail.com>
2024-10-13 17:33:11 +0400

    first commit

---
+++ day1/homework.py
@@ -0,0 +1,7 @@
+name="I am luka."
+surname="My surname is gorgadze."
+age="I am 15 years old."
+height="My height is 1.68."
+city="I live in Batumi."
+
+print( name +" "+ surname +" "+ age +" "+ height +" "+ city)
