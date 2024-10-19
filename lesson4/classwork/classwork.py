31904922bbd77134721eedcf1e933295020ca103
lukka77 <gorgadzeluka509@gmail.com>
2024-10-19 22:52:42 +0400

    second commit

---
+++ lesson4/classwork/classwork.py
@@ -0,0 +1,51 @@
+name="luka"
+surname="gorgadze"
+num=15
+num2=20
+num3=15.5
+num4=11.1
+
+print(name)
+print(surname)
+print(num)
+print(num2)
+print(num3)
+print(num4)
+
+print(25+30)
+print(33+77)
+print(55+55)
+print(13+66)
+print(90+210)
+
+print(33-11)
+print(46-22)
+print(87-45)
+print(12-5)
+print(99-33)
+
+print(5 * 9)
+print(12 * 4)
+print(4 * 8)
+print(6 * 0)
+print(4 * 9)
+
+print(35 / 7)
+print(56 / 8)
+print(36 / 6)
+print(100 / 2)
+print(4 / 2)
+
+print(5 % 3)
+print(10 & 4)
+print(49 & 6)
+print(35 & 4)
+print(27 & 6)
+
+print(2 ** 5)
+print(5 ** 9)
+print(4 ** 5)
+print(7 ** 6)
+print(8 ** 5)
+
+print((50 + 75) - (25 / 2) % (-6) ** 5)
