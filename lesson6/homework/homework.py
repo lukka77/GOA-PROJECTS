694509b6975ad654d43147df8cb234f8d88bef13
lukka77 <gorgadzeluka509@gmail.com>
2024-10-26 02:32:01 +0400

    second commit

---
+++ lesson6/homework/homework.py
@@ -0,0 +1,249 @@
+# +
+a=34
+a +=67
+print(a)
+
+a1=25
+a1 +=45
+print(a1)
+
+b=55
+b +=13
+print(b)
+
+b1=32
+b1 +=68
+print(b1)
+
+c=102
+c+=140
+print(c)
+
+c1=37
+c1 +=90
+print(c1)
+
+d=450
+d +=555
+print(d)
+
+d1=78
+d1 +=80
+print(d1)
+
+e=230
+e +=345
+print(e)
+
+e1=21
+e1 +=43
+print(e1)
+
+# -
+f=120
+f -=30
+print(f)
+
+f1=35
+f1 -=20
+print(20)
+
+g=90
+g -=45
+print(g)
+
+g1=76
+g1-=47
+print(g1)
+
+h=50
+h -=25
+print(h)
+
+h1=43
+h1 -=12
+print(h1)
+
+i=40
+i -=23
+print(i)
+
+i1=99
+i1 -=55
+print(i1)
+
+j=30
+j -=15
+print(j)
+
+j1=83
+j1 -=24
+print(j1)
+
+
+# *
+k=10
+k *=50
+print(k)
+
+k1=67
+k1 *=5
+print(k1)
+
+l=5
+l *=25
+print(l)
+
+l1=7
+l1 *=49
+print(l1)
+
+m=6
+m *=30
+print(m)
+
+m1=4
+m1 *=24
+print(m1)
+
+n=4
+n *=16
+print(n)
+
+n1=9
+n1 *=45
+print(n1)
+
+o=2
+o *=20
+print(o)
+
+o1=56
+o1 *=8
+print(o1)
+
+
+# /
+p=56
+p /=7
+print(p)
+
+p1=75
+p1 /=5
+print(p1)
+
+q=35
+q /=7
+print(q)
+
+q1=90
+q1 /=2
+print(q1)
+
+r=40
+r /=8
+print(r)
+
+r1=5
+r1 /=2
+print(r1)
+
+s=64
+s /=8
+print(s)
+
+s1=80
+s1 /=5
+print(s1)
+
+t=20
+t /=5
+print(t)
+
+t1=30
+t1 /=10
+print(t1)
+
+
+# %
+u=120
+u %=25
+print(u)
+
+u1=300
+u1 %=15
+print(u1)
+
+v=150
+v %=35
+print(v)
+
+v1=200
+v1 %=75
+print(v1)
+
+w=100
+w %=15
+print(w)
+
+w1=500
+w1 %=50
+print(w1)
+
+x=200
+x %=55
+print(x)
+
+x1=350
+x1 %=15
+print(x1)
+
+y=250
+y %=75
+print(y)
+
+y1=100
+y1 %=20
+print(y1)
+
+
+# **
+z=4
+z **=4
+print(z)
+
+z1=5
+z1 **=5
+print(z1)
+
+z2=6
+z2 **=6
+print(z2)
+
+z3=7
+z3 **=7
+print(z3)
+
+z4=8
+z4 **=8
+print(z4)
+
+z5=9
+z5 **=9
+print(z5)
+
+z6=10
+z6 **=10
+print(z6)
+
+z7=12
+z7 **=12
+print(z7)
+
+z8=14
+z8 **=14
+print(z8)
+
+z9=15
+z9 **=15
+print(z9)
