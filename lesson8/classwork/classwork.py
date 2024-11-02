cb483eeb63cdee3c80daea9bc51f3bc0b79308ed
lukka77 <gorgadzeluka509@gmail.com>
2024-11-02 17:32:11 +0400

    second commit

---
+++ lesson8/classwork/classwork.py
@@ -0,0 +1,19 @@
+name=input("Enter your name: ")
+surname=input("Enter your surname: ")
+age=input("Enter your age: ")
+number1=input("Enter the first number: ")
+number2=input("Enter the second number: ")
+
+
+print("Name:", name)
+print("Surname:", surname)
+print("Age:", age)
+print("Number 1:", number1)
+print("Number 2 :", number2 )
+
+
+print(type(name))
+print(type(surname))
+print(type(age))
+print(type(number1))
+print(type(number2))
---
+++ lesson8/homework/homework2.py
@@ -0,0 +1,48 @@
+# First Example
+num=150
+num2=20
+num3=2.5
+num4=11.1
+num5=120
+
+print(( num + num2 ) / num3 - ( num4 * num3 ) + ( num5 ** num2 ) % num5 )
+
+
+# Second Example
+num6=350
+num7=50
+num8=5
+num9=5.5
+num10=100
+
+print(( num6 + num8 ) * num7 - ( num10 / num7 ) + ( num10 % num7 ) ** num7 )
+
+
+# Third Example
+num11=250
+num12=250
+num13=5
+num14=7.5
+num15=150
+
+print(( num11 + num12 ) / num13 - ( num13 * num14 ) + ( num15 ** num12 ) % num15 )
+
+
+# Fourth Example
+num16=500
+num17=250
+num18=10
+num19=4,5
+num20=25
+
+print(( num16 / num17 ) * num18 - ( num20 + num18 ) + ( num20 ** num17 ) % num20 )
+
+
+# Fifth Example
+num21=100
+num22=10
+num23=35
+num24=3,5
+num25=35
+
+print(( num21 / num22 ) * num23 - ( num25 + num23 ) + ( num25 ** num22 ) % num25 )
