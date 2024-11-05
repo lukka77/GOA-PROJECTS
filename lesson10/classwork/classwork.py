d4ad7076d98692a07d76244e1f484b96ad2ae2df
lukka77 <gorgadzeluka509@gmail.com>
2024-11-05 23:02:14 +0400

    second commit

---
+++ lesson10/classwork/classwork.py
@@ -0,0 +1,13 @@
+print(float(5.5))
+str1 = str(2)
+print(type(str1))
+print(str1 + "15")
+
+number=float(input("Enter your number: "))
+print(type(number))
+print(number)
+
+number=int(input("Enter your name: "))
+print(type(number))
+print(number)
+
