2af9c28bef5ec164a5dc6fcd5520ed5b0fc16a68
lukka77 <gorgadzeluka509@gmail.com>
2024-11-08 23:30:12 +0400

    second commit

--- lesson10/classwork/classwork.py
+++ lesson10/classwork/classwork.py
@@ -3,6 +3,8 @@ str1 = str(2)
 print(type(str1))
 print(str1 + "15")
 
+
+# საკლასო დავალება 1
 number=float(input("Enter your number: "))
 print(type(number))
 print(number)
@@ -11,3 +13,114 @@ number=int(input("Enter your name: "))
 print(type(number))
 print(number)
 
+
+# საკლასო დავალება 2
+
+# მომხმარებლის წონისა და სიმაღლის მოთხოვნა
+weight = float(input("Enter your weight: "))
+height = float(input("Enter your height: "))
+bmi = weight / (height ** 2)
+print(bmi) 
+
+# საკლასო დავალება 3
+
+# >
+comprasion= 6 > 3
+print(comprasion)
+
+comprasion= 8 > 7
+print(comprasion)
+
+comprasion= 3 > 1
+print(comprasion)
+
+comprasion= 15 > 11
+print(comprasion)
+
+comprasion= 4 > 2
+print(comprasion)
+
+
+# <
+comprasion= 5 < 3
+print(comprasion)
+
+comprasion= 6 < 3
+print(comprasion)
+
+comprasion= 9 < 12
+print(comprasion)
+
+comprasion= 4 < 7
+print(comprasion)
+
+comprasion= 7 < 15
+print(comprasion)
+
+
+# >=
+comprasion= 6 >+ 6
+print(comprasion)
+
+comprasion= 23 >= 12
+print(comprasion)
+
+comprasion= 21 >= 17
+print(comprasion)
+
+comprasion= 35 >= 27
+print(comprasion)
+
+comprasion= 66 >= 66
+print(comprasion)
+
+
+# <=
+comprasion= 55 <= 67
+print(comprasion)
+
+comprasion= 17 <= 34
+print(comprasion)
+
+comprasion= 97 <= 120
+print(comprasion)
+
+comprasion= 42 <= 74
+print(comprasion)
+
+comprasion= 71 <= 71
+print(comprasion)
+
+
+# ==
+comprasion= 55 == 54
+print(comprasion)
+
+comprasion= 34 == 34
+print(comprasion)
+
+comprasion= 97 == 98
+print(comprasion)
+
+comprasion= 42 == 42
+print(comprasion)
+
+comprasion= 71 == 73
+print(comprasion)
+
+
+# !=
+comprasion= 50 != 52
+print(comprasion)
+
+comprasion= 35 != 34
+print(comprasion)
+
+comprasion= 13 != 15
+print(comprasion)
+
+comprasion= 22 != 45
+print(comprasion)
+
+comprasion= 130 != 240
+print(comprasion)
