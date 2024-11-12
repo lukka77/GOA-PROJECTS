c26cf37feb01940ed6567c222df5a9a62c595d9a
lukka77 <gorgadzeluka509@gmail.com>
2024-11-13 00:16:41 +0400

    second commit

---
+++ level12/classwork/classwork1.py
@@ -0,0 +1,13 @@
+# საკლასო დავალება: True, False ბულეანებზე ჩამოწერეთ 4 კომბინაცია.
+# ეს გააკეთეთ ან ოპერატორზეც და და ოპერატორზეც.
+# თითეოული კომბინაციის შედეგი უნდა დაიბეჭდოს და მარჯვნივ მიუწერეთ კომენტარის სახით შედეგი რაც არის
+
+print(True or True) #True
+print(False or True) # True
+print(True or False) # True
+print(False or False) # True
+
+print(True and True) # True
+print(False and True) # False
+print(True and False) # False
+print(False and False) # False
