2b456fb608dec9be1287793bdcfd441d49171ba8
lukka77 <gorgadzeluka509@gmail.com>
2024-11-17 18:43:26 +0400

    second commit

--- level12/homework/homework1.py
+++ level12/homework/homework1.py
@@ -1,13 +1,4 @@
-# საკლასო დავალება: True, False ბულეანებზე ჩამოწერეთ 4 კომბინაცია.
-#  ეს გააკეთეთ ან ოპერატორზეც და და ოპერატორზეც.
-# თითეოული კომბინაციის შედეგი უნდა დაიბეჭდოს და მარჯვნივ მიუწერეთ კომენტარის სახით შედეგი რაც არის
+# იმუშავეთ ლოგიკურ ოპერაციებზე: print ბრძანებას გადაეცით 2 ან 3 ბულეანი და 
+# ისინი ერთმანეთთან დააკავშირეთ ლოგიკური ოპერატორებით (or / and)
 
-print(True or True) #True
-print(False or True) # True
-print(True or False) # True
-print(False or False) # True
-
-print(True and True) # True
-print(False and True) # False
-print(True and False) # False
-print(False and False) # False
+print()
---
+++ level12/homework/homework4.py
@@ -0,0 +1,12 @@
+#  თქვენი სიტყვებით ახსენით რა არის control flow და რამდენი ტექნიკა გააჩნია მას
+
+print(" control flow-ს გააჩნია 3 ტექნიკა ესენია: თანმიმდევრობა(Sequences), ინტერაცია(Iterations) და პრიბოთი განცხადება(Selections).")
+print(" Sequences- ჯერ ერთი დავალება სრულდება, შემდეგ კი მეორე.")
+print(" Iterations- განმეორება, თუ მითითბული დავალება სწორად არ შესრულდება, გაბრუნებს იქიდან საიდანაც ეს დავალება დაიწყო.")
+print(" Selections- ამის საშვალებით მოწმდება პრიობა. ")
+
+# მოვიყვან ასეთ მაგალითს
+
+num=int(input("Enter first number: "))     # 1
+num1=int(input("Enter second number: "))   # 2
+print(num > 20 and num1 < 25)              # 3
