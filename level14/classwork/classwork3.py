cbaddfeaea0ce7b43401b77ccf754b5385bb0332
lukka77 <gorgadzeluka509@gmail.com>
2024-11-23 17:40:25 +0400

    second commit

---
+++ level14/classwork/classwork3.py
@@ -0,0 +1,10 @@
+# Task: Print the Squares of Numbers
+# Write a Python program that:
+
+# Asks the user to enter a number 𝑛
+# Uses a for loop to print the square of each number from 1 to n.
+
+n = int(input("Enter number: "))
+for i in range(n):
+    print(i**2)
+    print(i*i)
