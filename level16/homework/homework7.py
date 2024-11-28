f3b47b3d4a536e4f46a0c814a854d683ff3101c4
lukka77 <gorgadzeluka509@gmail.com>
2024-11-29 01:26:35 +0400

    second commit

---
+++ level16/homework/homework7.py
@@ -0,0 +1,8 @@
+# 7) Create a countdown from 10 to 1 using a while loop, and print "Blast off!" when the countdown finishes.
+
+i = 10
+while i > 0:
+    print(i)
+    i -= 1
+
+print("Blast off!")
---
+++ level16/homework/homework8.py
@@ -0,0 +1,11 @@
+# 8) Prompt the user to enter a password. Keep asking until they enter the correct password.
+
+correct_password = "12345678"
+
+user_input = input("Enter the password: ")
+
+while user_input != correct_password:
+    print("Incorrect password. Try again.")
+    user_input = input("Enter the password: ")
+
+print("Access!")
