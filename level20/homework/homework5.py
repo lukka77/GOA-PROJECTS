7e1037ad5ca144845ac1cbd9821cdb0aa07f238d
lukka77 <gorgadzeluka509@gmail.com>
2024-12-12 23:03:05 +0400

    second commit

---
+++ level20/homework/homework5.py
@@ -0,0 +1,9 @@
+# 5) Write a program that takes a number as input and prints whether it is positive, negative, or zero.
+
+num=int(input("Enter number:"))
+
+if num>0:
+    print("positive")
+
+elif num<0:
+    print("negative")
