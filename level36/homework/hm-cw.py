6ee825fc4363f96960f469d3d3ef33e365b3a8b3
lukka77 <gorgadzeluka509@gmail.com>
2025-02-10 00:01:51 +0400

    second commit

---
+++ level36/homework/hm-cw.py
@@ -0,0 +1,7 @@
+# 1) Given an array of integers your solution should find the smallest integer.
+
+def find_smallest_int(arr):
+    return min(arr)
+arr=[6, 3, 7, 9, 1, 2]
+smallest=find_smallest_int(arr)
+
