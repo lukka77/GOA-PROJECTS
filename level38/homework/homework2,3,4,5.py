c9020bf59f781f741a26f2b3ddeba6e7780deea6
lukka77 <gorgadzeluka509@gmail.com>
2025-02-14 00:50:31 +0400

    second commit

---
+++ level38/homework/homework2,3,4,5.py
@@ -0,0 +1,20 @@
+# 2) Tuple Indexing & Slicing: Create a tuple with at least 5 elements and extract the second, last, and a slice of the middle three elements.
+
+# 3) Tuple Immutability: Try to modify an element in a tuple and observe what happens.
+
+# 4) Tuple Packing & Unpacking: Create a tuple with multiple values, unpack them into separate variables, and print each variable.
+
+# 5) Tuple Methods: Use the .count() and .index() methods on a tuple containing repeated elements to count occurrences and find the first occurrence of a specific value.
+
+
+
+
+#2
+my_tuple = (10, 20, 30, 40, 50)
+second_element = my_tuple[1]
+last_element = my_tuple[-1]
+middle_slice = my_tuple[1:4]
+
+print("Second element:", second_element)
+print("Last element:", last_element)
+print("Middle three elements:", middle_slice)
